ff83814f54e42e266936749f3e2187aeed329b8c
atsushi-kume <atsushikume-online@memoad.jp>
2026-05-05 18:25:57 +0900

    getzipmain.js を更新

--- api/modules/getzipmain.js
+++ api/modules/getzipmain.js
@@ -1,17 +1,32 @@
-// 1. importは必ず一番上に書く
 import { ZipToAddress } from "../services/zipToAddress.js";
 import { AddressToZip } from "../services/addressToZip.js";
 
-// 2. 不要な function 宣言（1行目）を消すか、正しく閉じる
 export const GeoApi = {
   async execute(input) {
-    // 郵便番号(数値)か住所(オブジェクト)かで振り分け
+
+    // 全角→半角変換
+    if (typeof input === "string") {
+      input = input.replace(/[０-９]/g, s =>
+        String.fromCharCode(s.charCodeAt(0) - 0xFEE0)
+      );
+    }
+
+    // 郵便番号
     if (typeof input === "string" && /^\d{3}-?\d{4}$/.test(input)) {
       return await ZipToAddress.fetch(input);
     }
-    if (typeof input === "object") {
+
+    // 住所
+    if (
+      typeof input === "object" &&
+      input !== null &&
+      "pref" in input &&
+      "city" in input &&
+      "town" in input
+    ) {
       return await AddressToZip.fetch(input.pref, input.city, input.town);
     }
+
     throw new Error("入力形式が不正です");
   }
 };

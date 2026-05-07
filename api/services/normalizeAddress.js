d763d43d1cdf37d57dd556a77e122cf2d928d9d3
atsushi-kume <atsushikume-online@memoad.jp>
2026-05-07 14:14:36 +0900

    normalizeAddress.js を更新

--- api/services/normalizeAddress.js
+++ api/services/normalizeAddress.js
@@ -1,9 +1,14 @@
 export const NormalizeAddress = {
   async fetch(address) {
 
+    // 数字以降削除（表記揺れ低減）
     address = address.replace(/[0-9０-９\-－].*$/, "");
 
-    const url = `https://geoapi.heartrails.com/api/json?method=suggest&keyword=${encodeURIComponent(address)}&matching=like`;
+    const url =
+      `https://geoapi.heartrails.com/api/json` +
+      `?method=suggest` +
+      `&keyword=${encodeURIComponent(address)}` +
+      `&matching=like`;
 
     const res = await fetch(url);
     const data = await res.json();
@@ -16,13 +21,33 @@ export const NormalizeAddress = {
       throw new Error("該当住所なし");
     }
 
-    const loc = list[0];
+    // -----------------------------
+    // 正規化候補選定
+    // -----------------------------
+    const targetTown = extractTown(address);
+
+    const loc =
+      list.find(x => x.town === targetTown) ||
+      list.find(x => x.town.startsWith(targetTown)) ||
+      list.find(x => !x.town.includes("ビル")) ||
+      list[0];
+
+    console.log("採用候補:", loc);
 
     return {
       pref: loc.prefecture,
       city: loc.city,
-      town: loc.town || "",
-      postal: loc.postal || null   // ←追加
+      town: loc.town
     };
   }
 };
+
+// ------------------------------------
+// 町域抽出
+// ------------------------------------
+function extractTown(address) {
+  const match =
+    address.match(/(?:都|道|府|県).+?[市区町村](.+)$/);
+
+  return match ? match[1] : address;
+}
